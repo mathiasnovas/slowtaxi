c6efdc2fab6fa1d3932a1a93714276438943d9ff
Mathias Novas <mathias.novas@try.no>
2026-05-01 16:32:53 +0200

    Add Vercel Speed Insights

--- app/(blog)/layout.tsx
+++ app/(blog)/layout.tsx
@@ -1,4 +1,5 @@
 import { GoogleAnalytics } from "@next/third-parties/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Link from "next/link";
@@ -103,6 +104,7 @@ export default function BlogLayout({
           </div>
         </footer>
       </body>
+      <SpeedInsights />
       {process.env.NODE_ENV === "production" &&
         process.env.NEXT_PUBLIC_GA_ID && (
           <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
